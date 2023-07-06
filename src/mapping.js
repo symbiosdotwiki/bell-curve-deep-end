49a1f7f1e6d62047c6db7caed6491743bb5a0d33
N <nic@symbios.wiki>
2023-07-07 00:00:58 +0200

    boop

---
+++ src/mapping.js
@@ -0,0 +1,13 @@
+export const mapping = {
+	'Anemone' : 11,
+	'Coral' : 1,
+	'Crab' : 2,
+	'Shark' : 3,
+	'Dolphin' : 4,
+	'Seahrose' : 5,
+	'Pufferfish' : 6,
+	'Clownfish' : 7,
+	'Shell' : 8,
+	'Turtle' : 9,
+	'Jellyfish' : 10,
+}
