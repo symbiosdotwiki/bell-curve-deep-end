4e1d66e9d3a0afad09738836dbb65b36867dc873
N <nic@symbios.wiki>
2023-07-06 23:54:10 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { Suspense, useRef } from "react"
+import { Canvas, useLoader } from '@react-three/fiber'
+import { Environment } from '@react-three/drei'
+import ReactPlayer from 'react-player/soundcloud'
 
-function App() {
+import { useStore } from './state'
+import Scene from './Scene'
+
+import logo from './logo.svg'
+import './App.css'
+
+function Loading() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="LOADING">
+      LOADING
     </div>
-  );
+  )
 }
 
-export default App;
+function App() {
+  const scRef = useRef()
+  const curTrack = useStore((state) => state.curTrack)
+
+  if(curTrack && scRef.current)
+    scRef.current.getInternalPlayer().skip(curTrack)
+
+  return (
+    <div className="App">
+      <header> <meta name="viewport" content="width=device-width, initial-scale=1.0"/> </header>
+      <ReactPlayer 
+        className='sc-player'
+        url='https://soundcloud.com/isaka_symbios_wiki/sets/traxxx' 
+        width={'100%'}
+        height={'10%'}
+        ref={scRef}
+        config={{
+          playing:true,
+          soundcloud:{
+            options:{
+              start_track:curTrack
+            }
+          }
+        }}
+      />
+      <Suspense fallback={<Loading/>}>
+        <div className="THREE">
+          <Canvas shadows={false}>
+            <Scene
+              // resetCam={() => resetCam()}
+              // portrait={dimensions.portrait}
+            />
+            <Environment preset="sunset" background />
+          </Canvas>
+        </div>
+      </Suspense>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Scene.js
@@ -0,0 +1,42 @@
+import React, { useRef } from 'react'
+
+import { useFrame, useThree } from "@react-three/fiber"
+import { useGLTF } from '@react-three/drei'
+
+import { useStore } from './state'
+import { mapping } from './mapping'
+
+export default function Scene(props) {
+  const ref = useRef()
+  const gltf = useGLTF('/balloons.glb')
+  const { nodes, materials } = gltf
+
+  const curTrack = useStore((state) => state.curTrack)
+
+  useFrame(() => (ref.current.rotation.y += 0.002))
+  
+  useThree(({camera}) => {
+    camera.position.y = 5
+    camera.lookAt(0, 0, 0)
+  })
+
+  return (
+    <group ref={ref} {...props} dispose={null}>
+    {Object.keys(nodes).map((key,idx) => {
+      const geo = nodes[key]
+      const mat = materials[key]
+      return <primitive 
+        key={key}
+        object={geo} 
+        onClick={() => {
+          useStore.setState({'curTrack':mapping[key]})
+          
+          console.log(key, curTrack)
+        }}
+      />
+    })}
+    </group>
+  )
+}
+
+useGLTF.preload('/balloons.glb')
